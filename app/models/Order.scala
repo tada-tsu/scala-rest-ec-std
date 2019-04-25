b1349b946cc445d611a75dd4b1bf68d1629499e9
donuts-ono <ono.tadatsugu@donuts.ne.jp>
2019-04-25 19:00:29 +0900

    Add Order model

---
+++ app/models/Order.scala
@@ -0,0 +1,34 @@
+package models
+
+import play.api.libs.json.{JsValue, Json, Writes}
+import scalikejdbc._
+case class Order(
+                    id: Int,
+                    user_id: Int,
+                    status: String,
+                    amount: Int,
+                  )
+
+object Order extends SQLSyntaxSupport[Order] {
+  override val tableName = "orders"
+  val o = Order.syntax("o")
+
+  implicit val writes = new Writes[Order] {
+    def writes(o: Order): JsValue = {
+      Json.obj(
+        "id" -> o.id,
+        "key" -> o.setting_key,
+        "value" -> o.value,
+      )
+    }
+  }
+
+  def apply(r: ResultName[Order])(rs: WrappedResultSet): Order = Order(
+    rs.int(r.id),
+    rs.int(r.user_id),
+    rs.string(r.status),
+    rs.int(r.amount),
+  )
+
+  
+}
